2dadebf75bd13941a65c30a665bf1cb555ba841e
yolo4lifegod <30472599+yolo4lifegod@users.noreply.github.com>
2024-06-23 02:00:29 -0400

    Delete calendar_app directory

--- calendar_app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Create a blueprint named 'calendar'
-calendar_bp = Blueprint('calendar', __name__)
-
-# Import routes so they are registered with the blueprint
-from . import routes
